f146d21a3f67eaa5d20721468378a366c38ab726
Yesid Tombe <y.tombe@uniandes.edu.co>
2022-09-18 13:23:04 -0500

    Pruebas logica de supermercado

--- src/supermercado/supermercado.service.spec.ts
+++ src/supermercado/supermercado.service.spec.ts
@@ -1,9 +1,16 @@
+/* eslint-disable prettier/prettier */
+import { faker } from '@faker-js/faker';
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { TypeOrmTestingConfig } from '../shared/testing-utils/typeorm-testing-config';
+import { SupermercadoEntity } from './supermercado.entity';
 import { SupermercadoService } from './supermercado.service';
 
 describe('SupermercadoService', () => {
   let service: SupermercadoService;
+  let repository: Repository<SupermercadoEntity>;
+  let supermercadosList: SupermercadoEntity[];
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -12,9 +19,102 @@ describe('SupermercadoService', () => {
     }).compile();
 
     service = module.get<SupermercadoService>(SupermercadoService);
+    repository = module.get<Repository<SupermercadoEntity>>(getRepositoryToken(SupermercadoEntity));
+    await seedDatabase();
   });
+
+  const seedDatabase = async () => {
+    repository.clear();
+    supermercadosList = [];
+    for(let i = 0; i < 5; i++){
+        const supermercado: SupermercadoEntity = await repository.save({
+        nombre: faker.company.name(),
+        longitud: faker.address.longitude(),
+        latitud: faker.address.latitude(),
+        paginaweb: faker.internet.domainName()})
+        supermercadosList.push(supermercado);
+    }
+  }
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('findAll should return all supermercados', async () => {
+    const supermercados: SupermercadoEntity[] = await service.findAll();
+    expect(supermercados).not.toBeNull();
+    expect(supermercados).toHaveLength(supermercadosList.length);
+  });
+
+  it('findOne should return a supermercado by id', async () => {
+    const storedSupermercado: SupermercadoEntity = supermercadosList[0];
+    const supermercado: SupermercadoEntity = await service.findOne(storedSupermercado.id);
+    expect(supermercado).not.toBeNull();
+    expect(supermercado.nombre).toEqual(storedSupermercado.nombre);
+    expect(supermercado.longitud).toEqual(storedSupermercado.longitud);
+    expect(supermercado.latitud).toEqual(storedSupermercado.latitud);
+    expect(supermercado.paginaweb).toEqual(storedSupermercado.paginaweb);
+  });
+
+  it('findOne should throw an exception for an invalid supermercado', async () => {
+    await expect(() => service.findOne("0")).rejects.toHaveProperty("message", "El supermercado con el id dado no fue encontrado")
+  });
+
+  it('create should return a new supermercado', async () => {
+    const supermercado: SupermercadoEntity = {
+      id: '',
+      nombre: "Almacenes Jumbo",
+      longitud: faker.address.longitude(),
+      latitud: faker.address.latitude(),
+      paginaweb: faker.internet.domainName(),
+      ciudades: []
+    }
+ 
+    const newSupermercado: SupermercadoEntity = await service.create(supermercado);
+    expect(newSupermercado).not.toBeNull();
+ 
+    const storedSupermercado: SupermercadoEntity = await repository.findOne({where: {id: newSupermercado.id}})
+    expect(storedSupermercado).not.toBeNull();
+    expect(storedSupermercado.nombre).toEqual(newSupermercado.nombre);
+    expect(storedSupermercado.longitud).toEqual(newSupermercado.longitud);
+    expect(storedSupermercado.latitud).toEqual(newSupermercado.latitud);
+    expect(storedSupermercado.paginaweb).toEqual(newSupermercado.paginaweb);
+  });
+
+  it('update should modify a supermercado', async () => {
+    const supermercado: SupermercadoEntity = supermercadosList[0];
+    supermercado.nombre = "Supertiendas Olimpica";
+    supermercado.longitud = "-154.0226";
+    supermercado.latitud = '2.68452';
+    supermercado.paginaweb = 'uniandes.edu.co'
+    const updatedSupermercado: SupermercadoEntity = await service.update(supermercado.id, supermercado);
+    expect(updatedSupermercado).not.toBeNull();
+    const storedSupermercado: SupermercadoEntity = await repository.findOne({ where: { id: supermercado.id } })
+    expect(storedSupermercado).not.toBeNull();
+    expect(storedSupermercado.nombre).toEqual(supermercado.nombre)
+    expect(storedSupermercado.longitud).toEqual(supermercado.longitud)
+    expect(storedSupermercado.latitud).toEqual(supermercado.latitud);
+    expect(storedSupermercado.paginaweb).toEqual(supermercado.paginaweb);
+  });
+
+  it('update should throw an exception for an invalid supermercado', async () => {
+    let supermercado: SupermercadoEntity = supermercadosList[0];
+    supermercado = {
+      ...supermercado, nombre: "Olimpica", longitud: "-154.0226", latitud: "2.68452", paginaweb: "unicauca.edu.co"
+    }
+    await expect(() => service.update("0", supermercado)).rejects.toHaveProperty("message", "El supermercado con el id dado no fue encontrado")
+  });
+
+  it('delete should remove a supermercado', async () => {
+    const supermercado: SupermercadoEntity = supermercadosList[0];
+    await service.delete(supermercado.id);
+    const deletedSupermercado: SupermercadoEntity = await repository.findOne({ where: { id: supermercado.id } })
+    expect(deletedSupermercado).toBeNull();
+  });
+
+  it('delete should throw an exception for an invalid supermercado', async () => {
+    const supermercado: SupermercadoEntity = supermercadosList[0];
+    await service.delete(supermercado.id);
+    await expect(() => service.delete("0")).rejects.toHaveProperty("message", "El supermercado con el id dado no fue encontrado")
   });
 });
